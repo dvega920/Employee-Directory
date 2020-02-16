f9b51885652a06268a10bec34462d5076c648c9c
David Vega <dvega920@gmail.com>
2020-02-16 13:05:57 -0500

    added container class to wrapper component

--- employee_directory/src/components/Wrapper/wrapper.js
+++ employee_directory/src/components/Wrapper/wrapper.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Wrapper(props) {
     return (
-        <div className="wrapper">{props.children}</div>
+        <div className="wrapper container">{props.children}</div>
     )
 }
