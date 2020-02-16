4a19c6b0e6803bd36f35d98113c94349fabdd725
David Vega <dvega920@gmail.com>
2020-02-16 13:04:36 -0500

    added table to employee card component

--- employee_directory/src/components/EmployeeCard/employee.js
+++ employee_directory/src/components/EmployeeCard/employee.js
@@ -1,10 +1,31 @@
-import React from "react"
-import "./style.css"
+import React from "react";
+import "./style.css";
 
 export default function EmployeeCard(props) {
     return (
-        <div className="card p-3">
-            <h1>{props.children}</h1>
+        <div className="card mt-4">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Avatar</th>
+                        <th>Name</th>
+                        <th>Phone</th>
+                        <th>Email</th>
+                        <th>DOB</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{props.id}</td>
+                        <td><img alt={props.name} src={props.img} /></td>
+                        <td>{props.name}</td>
+                        <td>{props.phone}</td>
+                        <td>{props.email}</td>
+                        <td>{props.dob}</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
