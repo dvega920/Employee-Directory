3406758e0e796c72c17a419d41eb4484cce383df
David Vega <dvega920@gmail.com>
2020-02-16 15:05:24 -0500

    removed id field

--- employee_directory/src/App.js
+++ employee_directory/src/App.js
@@ -12,12 +12,13 @@ function App() {
       <Title>Employee Directory </Title>
       <Search></Search>
       <EmployeeCard
-        id={employees[0].id}
+        // id={employees[0].id}
         image={employees[0].image}
         name={employees[0].name}
         phone={employees[0].phone}
         email={employees[0].email}
         dob={employees[0].dob}
+
       />
     </Wrapper>);
 }
